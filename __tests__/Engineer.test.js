00bc4b343dc141d099c686d6f5df5a79b6613209
Jpeyton <jpwalk319@gmail.com>
2021-02-19 11:31:35 -0700

    built engineer class test suite

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,2 +1,25 @@
 const Employee = require('../lib/Employee');
 const Engineer = require('../lib/Engineer');
+
+describe('Engineer class', () => {
+    it('is instance of Employee', () => {
+        let engineer = new Engineer('testname', 'testid', 'testemail', 'testgithub');
+
+        expect(engineer instanceof Employee).toBe(true);
+    });
+    it('has github property', () => {
+        let engineer = new Engineer('testname', 'testid', 'testemail', 'testgithub');
+
+        expect(engineer.github).toBe('testgithub');
+    });
+    it('has working getGithub method', () => {
+        let engineer = new Engineer('testname', 'testid', 'testemail', 'testgithub');
+
+        expect(engineer.getGithub()).toBe('testgithub');
+    });
+    it('has working getRole method', () => {
+        let engineer = new Engineer('testname', 'testid', 'testemail', 'testgithub');
+
+        expect(engineer.getRole()).toBe('Engineer');
+    });
+});
