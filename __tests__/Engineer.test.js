92a56a7edef023e5ba2950a356c8012bc5cbb547
Jpeyton <jpwalk319@gmail.com>
2021-02-19 11:17:50 -0700

    added manager class to its JS module as well as a manager test suite

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,1 +1,2 @@
+const Employee = require('../lib/Employee');
 const Engineer = require('../lib/Engineer');
--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,1 +1,25 @@
+const Employee = require('../lib/Employee');
 const Manager = require('../lib/Manager');
+
+describe('Manager Class', () => {
+    it('is instance of Employee', () => {
+        let manager = new Manager('testname', 'testid', 'testemail', 'testofficenumber');
+
+        expect(manager instanceof Employee).toBe(true);
+    });
+    it('getRole returns manager', () => {
+        let manager = new Manager('testname', 'testid', 'testemail', 'testofficenumber');
+
+        expect(manager.getRole()).toBe('Manager');
+    });
+    it('has office number', () => {
+        let manager = new Manager('testname', 'testid', 'testemail', 'testofficenumber');
+
+        expect(manager.officeNumber).toBe('testofficenumber');
+    });
+    it('getofficeNumber returns officeNumber', () => {
+        let manager = new Manager('testname', 'testid', 'testemail', 'testofficenumber');
+
+        expect(manager.getOfficeNumber()).toBe('testofficenumber');
+    });
+});
