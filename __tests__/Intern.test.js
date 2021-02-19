7505429eefa5861ddd1b8c57e7b708a2c5a0409c
Jpeyton <jpwalk319@gmail.com>
2021-02-19 11:38:10 -0700

    built intern test suite

--- __tests__/Intern.test.js
+++ __tests__/Intern.test.js
@@ -1,2 +1,25 @@
 const Employee = require('../lib/Employee');
 const Intern = require('../lib/Intern');
+
+describe('Intern class', () => {
+    it('is instance of Employee', () => {
+        let intern = new Intern('testname', 'testid', 'testemail', 'school');
+
+        expect(intern instanceof Employee).toBe(true);
+    });
+    it('has school property', () => {
+        let intern = new Intern('testname', 'testid', 'testemail', 'school');
+
+        expect(intern.school).toBe('school');
+    });
+    it('has working getSchool method', () => {
+        let intern = new Intern('testname', 'testid', 'testemail', 'school');
+
+        expect(intern.getSchool()).toBe('school');
+    });
+    it('has working getRole method', () => {
+        let intern = new Intern('testname', 'testid', 'testemail', 'school');
+
+        expect(intern.getRole()).toBe('Intern');
+    });
+});
