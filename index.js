2ce281b638b163120dcf26425c8e2fe9a0c571e2
Jpeyton <jpwalk319@gmail.com>
2021-02-23 18:52:26 -0700

    added inquirer prompts to index js for creation of employees

--- index.js
+++ index.js
@@ -1,7 +1,97 @@
-const fs = require('fs');
-const inquirer = require('inquirer');
-const Employee = require('./lib/Employee');
-const Engineer = require('./lib/Engineer');
-const Intern = require('./lib/Intern');
-const Manager = require('./lib/Manager');
+const fs = require("fs");
+const inquirer = require("inquirer");
+const Employee = require("./lib/Employee");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const Manager = require("./lib/Manager");
 
+const employeeArr = [];
+
+const createManager = inquirer.prompt([
+  {
+    type: "input",
+    message: "What is the team manager's name?",
+    name: "name",
+  },
+  {
+    type: "input",
+    message: "At what office number can the manager be found?",
+    name: "officeNumber",
+  },
+  {
+    type: "input",
+    message: "What is their employee id number?",
+    name: "id",
+  },
+  {
+    type: "input",
+    message: "What is the manager's email address?",
+    name: "email",
+  },
+  {
+    type: 'list',
+    message: 'What kind of employee would you like to add next?',
+    name: 'next',
+    choices: ['Intern', 'Engineer', 'Finished']  
+  }
+]);
+
+const createEngineer = inquirer.prompt([
+  {
+    type: "input",
+    message: "What is the team engineer's name?",
+    name: "name",
+  },
+  {
+    type: "input",
+    message: "What is the engineer's github profile address?",
+    name: "github",
+  },
+  {
+    type: "input",
+    message: "What is their employee id number?",
+    name: "id",
+  },
+  {
+    type: "input",
+    message: "What is the engineer's email address?",
+    name: "email",
+  },
+  {
+    type: 'list',
+    message: 'What kind of employee would you like to add next?',
+    name: 'next',
+    choices: ['Intern', 'Engineer', 'Finished']  
+  }
+]);
+
+const createIntern = inquirer.prompt([
+  {
+    type: "input",
+    message: "What is the intern's name?",
+    name: "name",
+  },
+  {
+    type: "input",
+    message: "What school is the intern from?",
+    name: "school",
+  },
+  {
+    type: "input",
+    message: "What is their employee id number?",
+    name: "id",
+  },
+  {
+    type: "input",
+    message: "What is the intern's email address?",
+    name: "email",
+  },
+  {
+    type: 'list',
+    message: 'What kind of employee would you like to add next?',
+    name: 'next',
+    choices: ['Intern', 'Engineer', 'Finished']  
+  }
+]);
+
+module.exports = employeeArr;
