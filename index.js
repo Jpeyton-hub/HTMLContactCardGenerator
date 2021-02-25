9b39fa0a62f731531dd8b32b2a1452791a7f92d7
Jpeyton <jpwalk319@gmail.com>
2021-02-24 18:56:57 -0700

    changed prompt functions so that they call eachother based on the users selection of which employee to create next

--- index.js
+++ index.js
@@ -7,91 +7,152 @@ const Manager = require("./lib/Manager");
 
 const employeeArr = [];
 
-const createManager = inquirer.prompt([
-  {
-    type: "input",
-    message: "What is the team manager's name?",
-    name: "name",
-  },
-  {
-    type: "input",
-    message: "At what office number can the manager be found?",
-    name: "officeNumber",
-  },
-  {
-    type: "input",
-    message: "What is their employee id number?",
-    name: "id",
-  },
-  {
-    type: "input",
-    message: "What is the manager's email address?",
-    name: "email",
-  },
-  {
-    type: 'list',
-    message: 'What kind of employee would you like to add next?',
-    name: 'next',
-    choices: ['Intern', 'Engineer', 'Finished']  
-  }
-]);
+function createManager() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "What is the team manager's name?",
+        name: "name",
+      },
+      {
+        type: "input",
+        message: "At what office number can the manager be found?",
+        name: "officeNumber",
+      },
+      {
+        type: "input",
+        message: "What is their employee id number?",
+        name: "id",
+      },
+      {
+        type: "input",
+        message: "What is the manager's email address?",
+        name: "email",
+      },
+      {
+        type: "list",
+        message: "What kind of employee would you like to add next?",
+        name: "next",
+        choices: ["Intern", "Engineer", "Finished"],
+      },
+    ])
+    .then((res) => {
+      employeeArr.push(
+        new Manager(res.name, res.id, res.email, res.officeNumber)
+      );
+      console.log(employeeArr);
+      switch (res.next) {
+        case "Intern":
+          createIntern();
+          break;
 
-const createEngineer = inquirer.prompt([
-  {
-    type: "input",
-    message: "What is the team engineer's name?",
-    name: "name",
-  },
-  {
-    type: "input",
-    message: "What is the engineer's github profile address?",
-    name: "github",
-  },
-  {
-    type: "input",
-    message: "What is their employee id number?",
-    name: "id",
-  },
-  {
-    type: "input",
-    message: "What is the engineer's email address?",
-    name: "email",
-  },
-  {
-    type: 'list',
-    message: 'What kind of employee would you like to add next?',
-    name: 'next',
-    choices: ['Intern', 'Engineer', 'Finished']  
-  }
-]);
+        case "Engineer":
+          createEngineer();
+          break;
 
-const createIntern = inquirer.prompt([
-  {
-    type: "input",
-    message: "What is the intern's name?",
-    name: "name",
-  },
-  {
-    type: "input",
-    message: "What school is the intern from?",
-    name: "school",
-  },
-  {
-    type: "input",
-    message: "What is their employee id number?",
-    name: "id",
-  },
-  {
-    type: "input",
-    message: "What is the intern's email address?",
-    name: "email",
-  },
-  {
-    type: 'list',
-    message: 'What kind of employee would you like to add next?',
-    name: 'next',
-    choices: ['Intern', 'Engineer', 'Finished']  
-  }
-]);
+        case "Finished":
+          console.log("Team created!");
+      }
+    });
+}
+
+function createEngineer() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "What is the team engineer's name?",
+        name: "name",
+      },
+      {
+        type: "input",
+        message: "What is the engineer's github profile address?",
+        name: "github",
+      },
+      {
+        type: "input",
+        message: "What is their employee id number?",
+        name: "id",
+      },
+      {
+        type: "input",
+        message: "What is the engineer's email address?",
+        name: "email",
+      },
+      {
+        type: "list",
+        message: "What kind of employee would you like to add next?",
+        name: "next",
+        choices: ["Intern", "Engineer", "Finished"],
+      },
+    ])
+    .then((res) => {
+      employeeArr.push(new Engineer(res.name, res.id, res.email, res.github));
+      console.log(employeeArr);
+      switch (res.next) {
+        case "Intern":
+          createIntern();
+          break;
+
+        case "Engineer":
+          createEngineer();
+          break;
+
+        case "Finished":
+          console.log("Team created!");
+      }
+    });
+}
+
+function createIntern() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "What is the intern's name?",
+        name: "name",
+      },
+      {
+        type: "input",
+        message: "What school is the intern from?",
+        name: "school",
+      },
+      {
+        type: "input",
+        message: "What is their employee id number?",
+        name: "id",
+      },
+      {
+        type: "input",
+        message: "What is the intern's email address?",
+        name: "email",
+      },
+      {
+        type: "list",
+        message: "What kind of employee would you like to add next?",
+        name: "next",
+        choices: ["Intern", "Engineer", "Finished"],
+      },
+    ])
+    .then((res) => {
+      employeeArr.push(new Intern(res.name, res.id, res.email, res.school));
+      console.log(employeeArr);
+      switch (res.next) {
+        case "Intern":
+          createIntern();
+          break;
+
+        case "Engineer":
+          createEngineer();
+          break;
+
+        case "Finished":
+          console.log("Team created!");
+      };
+    });
+};
+
+createManager()
 
 module.exports = employeeArr;
